243b37cac15a7dc0a306d437b9d5e633eea685f4
manivannan-selvarajan <ms.manivannan1963@gmail.com>
2021-06-21 16:03:51 +0530

    Add mailgun mailservice provider

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,22 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+#ActionMailer::Base.smtp_settings = {
+#  :address => 'smtp.mailgun.org',
+#  :port => 587,
+#  :authentication => :plain,
+#  :user_name => ENV['MAILGUN_USERNAME'],
+#  :password => ENV['MAILGUN_PASSWORD'],
+#  :domin => 'heroku.com',
+#  :enable_starttle_auto => true
+#}
+
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'msm-photo-app.heroku.com',
   :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domin => 'heroku.com',
-  :enable_starttle_auto => true
 }
+ActionMailer::Base.delivery_method = :smtp
