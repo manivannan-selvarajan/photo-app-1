a8a01993ddbe17cb084080f6329919d940c19c37
manivannan-selvarajan <ms.manivannan1963@gmail.com>
2021-07-03 22:30:24 +0530

    Modification in mail gun validation

--- config/environment.rb
+++ config/environment.rb
@@ -4,23 +4,23 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
- :address => 'smtp.mailgun.org',
- :port => 587,
- :authentication => :plain,
- :user_name => ENV['MAILGUN_USERNAME'],
- :password => ENV['MAILGUN_PASSWORD'],
- :domin => 'heroku.com',
- :enable_starttle_auto => true
-}
-
 # ActionMailer::Base.smtp_settings = {
-#   :port           => ENV['MAILGUN_SMTP_PORT'],
-#   :address        => ENV['MAILGUN_SMTP_SERVER'],
-#   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-#   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-#   :domain         => 'msm-photo-app.heroku.com',
-#   :authentication => :plain,
+#  :address => 'smtp.mailgun.org',
+#  :port => 587,
+#  :authentication => :plain,
+#  :user_name => ENV['MAILGUN_USERNAME'],
+#  :password => ENV['MAILGUN_PASSWORD'],
+#  :domin => 'heroku.com',
+#  :enable_starttle_auto => true
 # }
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'msm-photo-app.heroku.com',
+  :authentication => :plain,
+}
 
 ActionMailer::Base.delivery_method = :smtp
